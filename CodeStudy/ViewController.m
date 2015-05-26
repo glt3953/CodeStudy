e2c04dfce55330904c566e86035aa522f15074be
glt3953 <395313544@qq.com>
2015-05-26 19:32:49 +0800

    优化排序代码，提高效率

--- CodeStudy/ViewController.m
+++ CodeStudy/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 //    NSMutableArray *array = @[@1, @12, @2, @4, @51, @66, @45, @25, @96, @78, @55, @23];
-    NSMutableArray *array = [NSMutableArray arrayWithArray:@[@1, @12, @2, @4, @51, @66, @45, @25, @96, @78, @55, @23]];
+    NSMutableArray *array = [NSMutableArray arrayWithArray:@[@13, @12, @2, @4, @51, @66, @45, @25, @96, @78, @55, @23]];
     [self bubbleSort:array];
 }
 
@@ -26,31 +26,31 @@
 - (void)bubbleSort:(NSMutableArray *)array {
     int num = 0; //循环次数
     
-    // 降序
-//    for (NSUInteger i = 0; i < array.count; i++) {
-//        for (NSUInteger j = i + 1; j < array.count; j++) {
-//            if (array[i] < array[j]) {
-//                id m = array[i];
-//                array[i] = array[j];
-//                array[j] = m;
-////                [array exchangeObjectAtIndex:j withObjectAtIndex:i];
-//                num++; //48次
-//            }
-//        }
-//    }
-    
     // 升序
-    int i, y;
-    BOOL bFinish = YES;
-    for (i = 1; i <= [array count] && bFinish; i++) {
-        bFinish = NO;
-        for (y = (int)[array count] - 1; y >= i; y--) {
-            if ([[array objectAtIndex:y] intValue] < [[array objectAtIndex:y-1] intValue]) { [array exchangeObjectAtIndex:y-1 withObjectAtIndex:y];
-                bFinish = YES;
-                num++; //18次
+    for (NSUInteger i = 1; i < array.count; i++) {
+        for (NSUInteger j = array.count - 1; j >= i; j--) {
+            if (array[j] < array[j - 1]) {
+                id m = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = m;
+//                [array exchangeObjectAtIndex:j withObjectAtIndex:i];
+                num++; //48次
             }
         }
     }
+    
+    // 升序
+//    int i, y;
+//    BOOL bFinish = YES;
+//    for (i = 1; i <= [array count] && bFinish; i++) {
+//        bFinish = NO;
+//        for (y = (int)[array count] - 1; y >= i; y--) {
+//            if ([[array objectAtIndex:y] intValue] < [[array objectAtIndex:y-1] intValue]) { [array exchangeObjectAtIndex:y-1 withObjectAtIndex:y];
+//                bFinish = YES;
+//                num++; //18次
+//            }
+//        }
+//    }
     
     NSLog(@"num:%d", num);
 }
